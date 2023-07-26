64ae4da1e4cc1399e2569b2dc2ee8749a77081da
Gregory Izuka <gizuka@seamfix.com>
2023-07-27 00:36:46 +0100

    added socket

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express')
 const http = require('http')
+const path = require('path');
+const socket = require('socket.io')
 
 const app = express()
 const port = 8000
 
+
 const server = http.createServer(app)
+
+// const socketServer = socket.server
+const io = socket(server)
+
+io.on('connection', function(socket){
+    console.log('new user connected')
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected')
+    })
+})
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
 
 server.listen(port, ()=>{
     console.log(`server running on ${port}`)
